f8caa4576b27fecdab1f7a9b6b2ad449b055181e
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-22 00:19:45 -0600

    add react bootstrap and first react-router route

---
+++ app/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>put components here</div>;
+};
+
+export default Home;
--- app/main.js
+++ app/main.js
@@ -3,10 +3,17 @@ import reactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '../store/store';
 import '../public/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Home } from './components';
 
 reactDOM.render(
   <Provider store={store}>
-    <div>put components here</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   </Provider>,
-  document.getElementById('app')
+  document.getElementById('root')
 );
