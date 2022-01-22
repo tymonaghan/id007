50ef8a53c37465031a0b0883b02c690d336ada5f
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-22 05:16:36 -0600

    swap incorrect boolean for volleyball logging

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // http logging
-if (process.env.DEPLOY) {
+if (!process.env.DEPLOY) {
   app.use(volleyball);
 }
 
