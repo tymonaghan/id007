4042af03fbc91c7bdb9f2dd3445d89dd9a3f5cea
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-21 23:02:31 -0600

    install webpack, express, react, and create start script

---
+++ app/main.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+reactDOM.render(<div>put components here</div>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'development',
+  entry: ['./app/main.js'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
